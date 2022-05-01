7008caad14ff7cb3bb36afa28112de01ba354d55
gusmendez99 <gus.mendez.99@gmail.com>
2022-05-01 02:20:29 -0600

    [ADD] sequential & parallel using c++11 threads

--- sequential.cpp
+++ sequential.cpp
@@ -1,5 +1,7 @@
-// 3_laba.cpp: ���������� ����� ����� ��� ����������� ����������.
-//
+/*
+	Code Reference: https://cryptopp.com/wiki/TripleDES
+    For cryptographic purposes a Crypto++ library was used (https://github.com/weidai11/cryptopp). 
+*/
 
 #include <iostream>
 using std::cout;
@@ -32,6 +34,14 @@ using CryptoPP::CBC_Mode;
 using CryptoPP::SecByteBlock;
 using CryptoPP::byte;
 
+#define UPPER_LIMIT 255
+#define LOWER_LIMIT 0
+#define STOP_SEARCH_COND 
+
+//! Hard-coded message to encrypt. It is used to validate the decryption output in brute-force method.
+const string PLAIN_TEXT = "In cryptography, a brute-force attack consists of an attacker submitting many passwords or passphrases with the hope of eventually guessing correctly.";
+const string PLAIN_TEXT_SEARCH = "attack";
+
 
 int main(int argc, char* argv[])
 {
@@ -39,19 +49,22 @@ int main(int argc, char* argv[])
 
 	SecByteBlock key(DES::DEFAULT_KEYLENGTH);
 	prng.GenerateBlock(key, key.size());
+	// Ir you dont want a PRNG, use this: 
+	// byte key[DES::KEYLENGTH] = {CHAR_MIN, CHAR_MIN, CHAR_MIN, CHAR_MIN, CHAR_MIN, CHAR_MIN, CHAR_MIN, CHAR_MIN};
 
 	byte iv[DES::BLOCKSIZE];
 	prng.GenerateBlock(iv, sizeof(iv));
 
-	string plain = "CBC Mode Test";
-	string cipher, encoded, recovered;
+	// Plain & Stop Search Condition
+	string cipher, encoded, candidate;
+	
 
 	/*********************************\
+	 * 
+	 * 			Encryption
+	 * 
 	\*********************************/
-
-    // cout << "key length: " << DES::DEFAULT_KEYLENGTH << endl;
-    // cout << "block size: " << DES::BLOCKSIZE << endl;
-
+	
 	// Pretty print key
 	encoded.clear();
 	StringSource(key, key.size(), true,
@@ -70,12 +83,9 @@ int main(int argc, char* argv[])
 	); // StringSource
 	cout << "iv: " << encoded << endl;
 
-	/*********************************\
-	\*********************************/
-
 	try
 	{
-		cout << "plain text: " << plain << endl;
+		cout << "plain text: " << PLAIN_TEXT << endl;
 
 		CBC_Mode< DES >::Encryption e;
 		e.SetKeyWithIV(key, key.size(), iv);
@@ -83,7 +93,7 @@ int main(int argc, char* argv[])
 		// The StreamTransformationFilter adds padding
 		//  as required. ECB and CBC Mode must be padded
 		//  to the block size of the cipher.
-		StringSource(plain, true, 
+		StringSource(PLAIN_TEXT, true, 
 			new StreamTransformationFilter(e,
 				new StringSink(cipher)
 			) // StreamTransformationFilter      
@@ -96,6 +106,9 @@ int main(int argc, char* argv[])
 	}
 
 	/*********************************\
+	 * 
+	 * 	  Decryption (Brute Force)
+	 * 
 	\*********************************/
 
 	// Pretty print
@@ -109,30 +122,63 @@ int main(int argc, char* argv[])
 
 	/*********************************\
 	\*********************************/
+	
+	CryptoPP::byte key_bf[DES::KEYLENGTH];
+	for(int i = 0; i < 8; i++) {
+		key_bf[i] = (CryptoPP::byte) 0;
+	};
 
 	try
 	{
-		CBC_Mode< DES >::Decryption d;
-		d.SetKeyWithIV(key, key.size(), iv);
+		for(int i = 0; i < 255; i++){
+			key_bf[0] = (byte) i;
+			for(int j = 0; j < 255; j++){
+				key_bf[1] = (byte) j;
+				for(int k = 0; k < 255; k++){
+					key_bf[2] = (byte) k;
+					for(int l = 0; l < 255; l++){
+						key_bf[3] = (byte) l;
+						for(int m = 0; m < 255; m++){
+							key_bf[4] = (byte) m;
+							for(int n = 0; n < 255; n++){
+								key_bf[5] = (byte) n;
+								for(int o = 0; o < 255; o++){
+									key_bf[6] = (byte) o;
+									for(int p = 0; p < 255; p++){
+										key_bf[7] = (byte) p;
 
-		// The StreamTransformationFilter removes
-		//  padding as required.
-		StringSource s(cipher, true, 
-			new StreamTransformationFilter(d,
-				new StringSink(recovered)
-			) // StreamTransformationFilter
-		); // StringSource
+										CBC_Mode<DES>::Decryption d;
+										d.SetKeyWithIV(key_bf, DES::KEYLENGTH, iv);
 
-		cout << "recovered text: " << recovered << endl;
+										// The StreamTransformationFilter removes
+										//  padding as required.
+										StringSource s(cipher, true, 
+											new StreamTransformationFilter(d,
+												new StringSink(candidate),
+												CryptoPP::BlockPaddingSchemeDef::ZEROS_PADDING 
+											) // StreamTransformationFilter
+										); // StringSource
+
+										// Check Stop Search Condition
+										if(candidate.find(PLAIN_TEXT_SEARCH) != string::npos)
+    									{
+											cout << "DES was successfully broken. Decipher text: " << candidate << endl;
+											return 0;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	catch(const CryptoPP::Exception& e)
 	{
 		cerr << e.what() << endl;
 		exit(1);
 	}
-
-	/*********************************\
-	\*********************************/
 
 	return 0;
 }
